988954fa8aa38576264c7d7b4ac9743ff389e085
Binoy <binoyvk99@gmail.com>
2023-05-18 00:31:12 +0530

    S1L13

--- index.js
+++ index.js
@@ -10,9 +10,115 @@
 //let myDogAge = myAge * humanDogRatio
 //console.log(myDogAge)
 
-let bonusPoints = 50
-bonusPoints = bonusPoints + 50
-bonusPoints = bonusPoints - 75
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
+//let bonusPoints = 50
+//bonusPoints = bonusPoints + 50
+//bonusPoints = bonusPoints - 75
+//bonusPoints = bonusPoints + 45
+//console.log(bonusPoints)
+
+//function increment() {
+//    console.log("The button was clicked.")    
+//}
+
+//function countdown() {
+//    console.log(5)
+//    console.log(4)
+//    console.log(3)
+//    console.log(2)
+//    console.log(1)
+//}
+
+//countdown() //setting up race
+//go
+//players are running the race
+//race is finished!
+
+//countdown() //new race setting up
+
+//function log_num() {
+//    console.log(42)
+//}
+
+//log_num()
+
+//let lap1 = 34
+//let lap2 = 33
+//let lap3 = 36
+
+//function lap_sum() {
+//    let laps = lap1 + lap2 + lap3;
+//    console.log(laps)
+//}
+
+//lap_sum()
+
+//let lapsCompleted = 0
+
+//function lapped() {
+//    lapsCompleted = lapsCompleted + 1
+//}
+
+//lapped()
+//lapped()
+//lapped()
+
+//console.log(lapsCompleted)
+let count = 0
+
+function increment() {
+    console.log("clicked")
+    count = count + 1
+    console.log(count)
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
